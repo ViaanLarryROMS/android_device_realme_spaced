b5fe51b6987795023a32ddb6d55eb6d6014d70de
ViaanROMS <viaan152013@gmail.com>
2026-01-29 09:42:36 +0530

    Update and rename lineage_spaced.mk to infinity_spaced.mk

--- infinity_spaced.mk
+++ infinity_spaced.mk
@@ -12,25 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/spaced/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Axion Stuff
-PRODUCT_NO_CAMERA := false
-AXION_CAMERA_REAR_INFO := 50,2,2
-AXION_CAMERA_FRONT_INFO := 16
-AXION_MAINTAINER := HELLINFIX
-AXION_PROCESSOR := MTK_Helio_G96
-
-# Axion Optional Flags
-TARGET_ENABLE_BLUR := true
-PERF_ANIM_OVERRIDE := true
-TARGET_USES_SLMK := true
-TARGET_INCLUDES_LOS_PREBUILTS := false
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_spaced
+PRODUCT_NAME := infinity_spaced
 PRODUCT_DEVICE := spaced
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
@@ -39,3 +26,17 @@ PRODUCT_MODEL := RMX3286
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 BUILD_FINGERPRINT := realme/RMX3286/RE54B4L1:13/SP1A.210812.016/R.1c05817+2a8bc:user/release-keys
+
+# Maintainer Name
+INFINITY_MAINTAINER := "DeezNutz"
+
+# Whether the device supports Fingerprint On Display
+TARGET_HAS_UDFPS := false
+
+# Whether Including Google Apps
+WITH_GAPPS := true
+
+PRODUCT_SYSTEM_PROPERTIES :=
+ro.product.marketname := Realme 8i
+ro.infinity.soc := MediaTek Helio G96
+ro.infinity.camera
