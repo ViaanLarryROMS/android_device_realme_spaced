d22f8bef1a5b19a0ceaad5b50d8f14cd1163efd9
ViaanROM <viaan152013@gmail.com>
2026-02-28 20:19:01 +0530

    Update lineage_spaced.mk

--- lineage_spaced.mk
+++ lineage_spaced.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/realme/spaced/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Include Dolby Atmos
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 PRODUCT_NAME := lineage_spaced
 PRODUCT_DEVICE := spaced
 PRODUCT_MANUFACTURER := realme
